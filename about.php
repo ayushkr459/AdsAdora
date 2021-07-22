fe41d12a9a2fb790e5bc51952c02a7973fcbadd0
Ayush Kumar <ayushkr459@gmail.com>
2021-07-23 03:37:01 +0530

    Updated Company Page

--- about.php
+++ about.php
@@ -1,13 +1,28 @@
 <?php
-  // include header.php
-  include('header.php');
+// include header.php
+include('header.php');
 ?>
 
-<div style="min-height: 100vh;" class="text-center mt-4">
-  <h3>About Us Page Blank Content</h3>
+<div class="mt-4">
+  <div class="container">
+    <h2><strong><u>About AdsAdora</u></strong></h2>
+    <hr>
+    <p style="margin-top: 2.5rem;">Welcome to Adsadora, your number one source for all Weekly Ads. We're dedicated to giving you the very best of Weekly Ads, with a focus on Latest &
+      Updated Weekly Ads that interests you and helps you save!
+    </p>
+    <p style="margin-top: 2.5rem;">Founded in 2021, Adsadora has come a long way from its beginnings in USA. When we first started out, Our passion for empowering people to shop smarter
+      drove them to monitor and update latest Weekly ads and flyers on a daily basis so that Adsadora can offer you the best local deals.
+      We now serve customers all over USA, and are thrilled that we're able to turn our passion into our own website.
+    </p>
+    <p style="margin-top: 2.5rem;">We hope you enjoy our ads as much as we enjoy offering them to you. If you have any questions or comments, please don't hesitate to <a href="contact.php" style="color: #dc3545;">Contact Us</a>.
+    </p>
+    <p style="margin-top: 2.5rem; margin-bottom: 2.5rem;">Sincerely, <br>
+      Adsadora Team
+    </p>
+  </div>
 </div>
 
 <?php
-  // include footer.php
-  include('footer.php');
+// include footer.php
+include('footer.php');
 ?>
--- privacy.php
+++ privacy.php
@@ -1,13 +1,75 @@
 <?php
-  // include header.php
-  include('header.php');
+// include header.php
+include('header.php');
 ?>
 
-<div style="min-height: 100vh;" class="text-center mt-4">
-  <h3>Privacy Page Blank Content</h3>
+<div style="min-height: 100vh;" class="mt-4 mb-4">
+  <div class="container">
+    <h2 style="margin-bottom: 1rem;"> <strong> <u>Privacy Policy</u> </strong></h3>
+    <p>At AdsAdora, accessible from <a href="https://adsadora.com/" target="_blank" rel="noopener noreferrer">https://adsadora.com/</a> , one of our main priorities is the privacy of our visitors. This Privacy Policy document contains
+      types of information that is collected and recorded by AdsAdora and how we use it.</p>
+    <p  >If you have additional questions or require more information about our Privacy Policy, do not hesitate to contact us.</p>
+    <p  >
+      This Privacy Policy applies only to our online activities and is valid for visitors to our website with regards to the information that they shared and/or collect in AdsAdora.
+      This policy is not applicable to any information collected offline or via channels other than this website.
+    </p>
+    <hr>
+    <h3 style="color:#dc3545;"> <strong> Consent </strong></h3>
+    <p  >
+      By using our website, you hereby consent to our Privacy Policy and agree to its terms.
+    </p>
+    <h3   style="color:#dc3545;"><strong> Information we collect </strong></h3>
+    <p  >The personal information that you are asked to provide, and the reasons why you are asked to provide it, will be made clear to you at the point we ask you to provide your personal information.
+      If you contact us directly, we may receive additional information about you such as your name, email address, phone number, the contents of the message and/or attachments you may send us, and any other information you may choose to provide.
+      When you register for an Account, we may ask for your contact information, including items such as name, company name, address, email address, and telephone number.
+    </p>
+    <h3   style="color:#dc3545;"> <strong> How we use your Information </strong></h3>
+    <p  >
+      We use the information we collect in various ways, including to:
+    </p>
+    <ul  >
+      <li>Provide, operate, and maintain our website</li>
+      <li>Improve, personalize, and expand our website</li>
+      <li>Understand and analyze how you use our website</li>
+      <li>Develop new products, services, features, and functionality</li>
+      <li>Communicate with you, either directly or through one of our partners, including for customer service, to provide you with updates and other information relating to the website, and for marketing and promotional purposes</li>
+      <li>Send you emails</li>
+      <li>Find and prevent fraud</li>
+    </ul>
+    <h3   style="color:#dc3545;"> <strong> Cookies and Web Beacons </strong></h3>
+    <p  >Like any other website, AdsAdora uses 'cookies'. These cookies are used to store information including visitors' preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the users' experience by customizing our web page content based on visitors' browser type and/or other information.
+      For more general information on cookies, please read "What Are Cookies" from Cookie Consent.
+    </p>
+    <h3   style="color:#dc3545;"><strong> Website Terms of Use </strong></h3>
+    <p  >
+      The AdsAdora website located at https://adsadora.com/ is a copyrighted work belonging to AdsAdora. Certain features of the Site may be subject to additional guidelines, terms, or rules, which will be posted on the Site in connection with such features.
+    </p>
+    <p  >
+      All such additional terms, guidelines, and rules are incorporated by reference into these Terms.
+    </p>
+    <p  >
+      These Terms of Use described the legally binding terms and conditions that oversee your use of the Site. BY LOGGING INTO THE SITE, YOU ARE BEING COMPLIANT THAT THESE TERMS and you represent that you have the authority and capacity to enter into these Terms. YOU SHOULD BE AT LEAST 18 YEARS OF AGE TO ACCESS THE SITE. IF YOU DISAGREE WITH ALL OF THE PROVISION OF THESE TERMS, DO NOT LOG INTO AND/OR USE THE SITE.
+    </p>
+    <p  >
+      These terms require the use of arbitration Section 10.2 on an individual basis to resolve disputes and also limit the remedies available to you in the event of a dispute.
+    </p>
+    <h3   style="color:#dc3545;"><strong>Access to the Site </strong> </h3>
+    <p  ><strong>Subject to these Terms.</strong> Company grants you a non-transferable, non-exclusive, revocable, limited license to access the Site solely for your own personal, noncommercial use.</p>
+    <p><strong>Certain Restrictions. </strong>The rights approved to you in these Terms are subject to the following restrictions: (a) you shall not sell, rent, lease, transfer, assign, distribute, host, or otherwise commercially exploit the Site; (b) you shall not change, make derivative works of, disassemble, reverse compile or reverse engineer any part of the Site; (c) you shall not access the Site in order to build a similar or competitive website; and (d) except as expressly stated herein, no part of the Site may be copied, reproduced, distributed, republished, downloaded, displayed, posted or transmitted in any form or by any means unless otherwise indicated, any future release, update, or other addition to functionality of the Site shall be subject to these Terms. All copyright and other proprietary notices on the Site must be retained on all copies thereof.
+      Company reserves the right to change, suspend, or cease the Site with or without notice to you. You approved that Company will not be held liable to you or any third-party for any change, interruption, or termination of the Site or any part.
+    </p>
+    <p  ><strong>No Support or Maintenance.</strong> You agree that Company will have no obligation to provide you with any support in connection with the Site.
+      Excluding any User Content that you may provide, you are aware that all the intellectual property rights, including copyrights, patents, trademarks, and trade secrets, in the Site and its content are owned by Company or Company’s suppliers. Note that these Terms and access to the Site do not give you any rights, title or interest in or to any intellectual property rights, except for the limited access rights expressed in Section 2.1. Company and its suppliers reserve all rights not granted in these Terms.
+    </p>
+    <hr>
+    <h3 style="color:#dc3545;"><strong> Disclaimers </strong></h3>
+    <p  > The site is provided on an "as-is" and "as available" basis, and company and our suppliers expressly disclaim any and all warranties and conditions of any kind, whether express, implied, or statutory, including all warranties or conditions of merchantability, fitness for a particular purpose, title, quiet enjoyment, accuracy, or non-infringement. We and our suppliers make not guarantee that the site will meet your requirements, will be available on an uninterrupted, timely, secure, or error-free basis, or will be accurate, reliable, free of viruses or other harmful code, complete, legal, or safe. If applicable law requires any warranties with respect to the site, all such warranties are limited in duration to ninety (90) days from the date of first use.
+      Some jurisdictions do not allow the exclusion of implied warranties, so the above exclusion may not apply to you. Some jurisdictions do not allow limitations on how long an implied warranty lasts, so the above limitation may not apply to you.
+    </p>
+  </div>
 </div>
 
 <?php
-  // include footer.php
-  include('footer.php');
+// include footer.php
+include('footer.php');
 ?>
