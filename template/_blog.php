9eb8eb997133f7452eb01999e5d6cab5e6baf8f1
Ayush Kumar <ayushkr459@gmail.com>
2021-07-27 15:43:25 +0530

    Blog Section Responsivness

--- template/_blog.php
+++ template/_blog.php
@@ -1,7 +1,10 @@
 <div class="container mt-2 mb-3">
+    <div class="row">
+
+    </div>
     <h3 class="text-center">Our Recent Blogs</h3>
     <div class="row justify-content-center mt-3 mb-5">
-        <div class="col-12 col-sm-8 col-md-6 col-lg-4 mt-2 d-flex">
+        <div class="col-sm-12 col-md-12 col-lg-4 mt-4 d-flex">
             <div class="card">
                 <img class="card-img img-fluid" src="./assets/images/target-blog-2.jpg" alt="image">
                 <div class="card-body text-center flex-fill">
@@ -13,7 +16,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-sm-8 col-md-6 col-lg-4 mt-2 d-flex">
+        <div class="col-sm-12 col-md-12 col-lg-4 mt-4 d-flex">
             <div class="card">
                 <img class="card-img img-fluid" src="./assets/images/walmart-blog-1.jpg" alt="image">
                 <div class="card-body text-center flex-fill">
@@ -25,7 +28,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 col-sm-8 col-md-6 col-lg-4 mt-2 d-flex">
+        <div class="col-sm-12 col-md-12 col-lg-4 mt-4 d-flex">
             <div class="card">
                 <img class="card-img" src="./assets/images/travel.jpg" alt="image">
                 <div class="card-body text-center flex-fill">
